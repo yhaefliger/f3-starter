27a7207cfd39d0805f263b0f5fc32014e56aa1c3
yaha <yann.haefliger@boas.ch>
2019-10-31 15:14:40 +0100

    update

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,5 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::create(__DIR__);
 $dotenv->load();
 
-$dsn = sprintf('mysql:dbname=%s;port=%s;host=%s', getenv('DB_NAME'), getenv('DB_PORT'), getenv('DB_HOST'));
-$db = new DB\SQL($dsn, getenv('DB_USER'), getenv('DB_PASS'));
+$dsn = sprintf('mysql:dbname=%s;port=%s;host=%s', env('DB_DATABASE', 'f3'), env('DB_PORT', '3306'), env('DB_HOST', '127.0.0.1'));
+$db = new DB\SQL($dsn, env('DB_USERNAME', 'root'), env('DB_PASSWORD', ''));
--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require __DIR__.'/../bootstrap.php';
 $f3 = \Base::instance();
 
 $f3->set('DB', $db);
+$f3->set('DEBUG', abs((int)env('DEBUG', 0)));
+
+$f3->config(__DIR__.'/../config/app.ini');
 
 $f3->route('GET /', function($f3){
     echo 'test';
