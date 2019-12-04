b02b21462015f3a10c1faf308f974bb4fd97501c
yaha <yann.haefliger@boas.ch>
2019-12-04 16:42:06 +0100

    added mix php func

---
+++ app/mix.php
@@ -0,0 +1,59 @@
+<?php
+if (! function_exists('starts_with')) {
+     /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    function starts_with($haystack, $needles){
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+if (! function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param string $path
+     * @param string $manifestDirectory
+     * @return string
+     *
+     * @throws \Exception
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+        static $manifest;
+        $publicFolder = '/public';
+        $rootPath = $_SERVER['DOCUMENT_ROOT'];
+        $publicPath = $rootPath . $publicFolder;
+        if ($manifestDirectory && ! starts_with($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+        if (! $manifest) {
+            if (! file_exists($manifestPath = ($rootPath . $manifestDirectory.'/mix-manifest.json') )) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+        }
+        
+        if (! starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        if (! array_key_exists($path, $manifest)) {
+            throw new Exception(
+                "Unable to locate Mix file: {$path}. Please check your ".
+                'webpack.mix.js output paths and try again.'
+            );
+        }
+        return file_exists($publicPath . ($manifestDirectory.'/hot'))
+                    ? "http://localhost:8080{$manifest[$path]}"
+                    : $manifestDirectory.$manifest[$path];
+    }
+}
