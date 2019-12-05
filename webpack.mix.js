9ec2c1ac1d97d5e6fc7cce38442f011a9b02428a
yaha <yann.haefliger@boas.ch>
2019-12-05 10:44:49 +0100

    added tailwind recommended plugins and  purge

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,23 @@
 const mix = require('laravel-mix');
-
+const purgecss = require('@fullhuman/postcss-purgecss')({
+   content: [
+      './resources/**/*.html'
+   ],
+   // Include any special characters you're using in this regular expression
+   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
+      require('postcss-import'),
       require('tailwindcss'),
+      require('postcss-preset-env')({ stage: 1 }),
+      ...process.env.NODE_ENV === 'production'
+         ? [purgecss]
+         : []
     ])
    .setPublicPath('public');
+
+if (mix.inProduction()) {
+   mix.version()
+}
