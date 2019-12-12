ae6813e9bae7d3926c99145c95e6bcdda1cded7f
yaha <yann.haefliger@boas.ch>
2019-12-12 13:36:52 +0100

    db pgsql+sqlite config

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,13 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$dsn = sprintf('mysql:dbname=%s;port=%s;host=%s', env('DB_DATABASE', 'f3'), env('DB_PORT', '3306'), env('DB_HOST', '127.0.0.1'));
-$db = new DB\SQL($dsn, env('DB_USERNAME', 'root'), env('DB_PASSWORD', ''));
+$connection = env('DB_CONNECTION');
+
+if($connection == 'mysql' || $connection == 'pgsql'){
+    $dsn = sprintf('%s:dbname=%s;port=%s;host=%s', $connection, env('DB_DATABASE', 'f3'), env('DB_PORT', '3306'), env('DB_HOST', '127.0.0.1'));
+    $db = new DB\SQL($dsn, env('DB_USERNAME', 'root'), env('DB_PASSWORD', ''));
+}elseif($connection == 'sqlite'){
+    $db = new DB\SQL(sprintf('sqlite:%s', env('DB_DATABASE', __DIR__.'/database.sqlite')));
+}else{
+    $db = false;
+}
