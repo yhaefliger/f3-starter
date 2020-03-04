f45a5df55d5a2fbb428de2226770f24b09f3289a
yaha <yann.haefliger@boas.ch>
2020-03-04 15:32:55 +0100

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,14 +4,19 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
       './resources/**/*.html'
    ],
    // Include any special characters you're using in this regular expression
-   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+   defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
 });
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
       require('postcss-import'),
       require('tailwindcss'),
-      require('postcss-preset-env')({ stage: 1 }),
+      require('postcss-preset-env')({ 
+         stage: 1,
+         features: {
+            'focus-within-pseudo-class': false
+          }
+      }),
       ...process.env.NODE_ENV === 'production'
          ? [purgecss]
          : []
